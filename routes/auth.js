70031ca9b83df34412c171232833bc71d383709a
Mayank Prasher <prasher6789@gmail.com>
2024-02-26 19:28:00 +0530

    project completed

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const {body} = require('express-validator');
 const User = require('../models/user');
 
 const authController = require('../controller/auth');
+const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.put('/signup',[
@@ -28,5 +29,14 @@ router.put('/signup',[
 ],
 authController.signup);
 router.post('/login',authController.login);
+
+router.get('/status',isAuth,authController.getUserStatus);
+
+router.patch('/status',isAuth,[
+    body('status')
+    .trim()
+    .not()
+    .isEmpty()
+],authController.updateUserStatus);
 
 module.exports = router;
